4fe22b5d920e7b8a3e937b942bf804e5750917cf
Luciano Ramella <lucianoramella13@gmail.com>
2024-03-04 20:31:22 -0300

    fix config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,15 +15,15 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://talleres123.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/estudio-de-la-fisica',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'talleres123', // Usually your GitHub org/user name.
-  projectName: 'Maxi_el_quimico', // Usually your repo name.
+  projectName: 'estudio-de-la-fisica', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
